bad0ea8234cc4f5c64fa36df2c151dc2bc0094aa
Rolf Heller <rolf.heller@ximedes.com>
2022-10-06 17:13:30 +0200

    looks like predict started working

--- docker/example.py
+++ docker/example.py
@@ -23,7 +23,9 @@ def signalToTensor(signal):
     filtered_snrs = collections.OrderedDict(sorted(filtered_signal.items()))
     if (len(filtered_snrs) != len(receivers)):
         raise("Not all receivers have a signal.")
-    return tf.constant(list(filtered_snrs.values()))/100
+    tensor = tf.constant(list(filtered_snrs.values()))/100
+    # print(tensor)
+    return tensor
 
 # Tuns the given stationname into a normalized Tensor.
 # The tensor contains the index of the name in the sorted list of stations,
@@ -33,7 +35,27 @@ def stationToTensor(stationName):
         index = stations.index(stationName)
     except:
         index = len(stations)
-    return tf.constant(index)
+    tensor = tf.constant(index)
+    # print(tensor)
+    return tensor
+
+# Splits the dataset into training, validation and test sets, 
+# see https://towardsdatascience.com/how-to-split-a-tensorflow-dataset-into-train-validation-and-test-sets-526c8dd29438
+def get_dataset_partitions_tf(ds, ds_size, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+    assert (train_split + test_split + val_split) == 1
+    
+    if shuffle:
+        # Specify seed to always have the same split distribution between runs
+        ds = ds.shuffle(shuffle_size, seed=12)
+    
+    train_size = int(train_split * ds_size)
+    val_size = int(val_split * ds_size)
+    
+    train_ds = ds.take(train_size)    
+    val_ds = ds.skip(train_size).take(val_size)
+    test_ds = ds.skip(train_size).skip(val_size)
+    
+    return train_ds, val_ds, test_ds
 
 # Example data (this is what we need to build, with information from the svxlink snr values
 # and manual classification of the audio.
@@ -43,11 +65,66 @@ def stationToTensor(stationName):
 # and then build a set like this:
 inputdata = {
     "John": {"Groningen": 50, "Leeuwarden": 43, "Smilde": 29, "Goes": 12, "Ignored": 123},
-    "Peter": {"Groningen": 5, "Leeuwarden": 90, "Smilde": 1, "Goes": 3, "Ignored": 123}
+    "Peter": {"Groningen": 5, "Leeuwarden": 90, "Smilde": 1, "Goes": 3, "Ignored": 123},
+    "John": {"Groningen": 50, "Leeuwarden": 43, "Smilde": 29, "Goes": 12, "Ignored": 123},
+    "Peter": {"Groningen": 5, "Leeuwarden": 90, "Smilde": 1, "Goes": 3, "Ignored": 123},
+    "John": {"Groningen": 50, "Leeuwarden": 43, "Smilde": 29, "Goes": 12, "Ignored": 123},
+    "Peter": {"Groningen": 5, "Leeuwarden": 90, "Smilde": 1, "Goes": 3, "Ignored": 123},
+    "John": {"Groningen": 50, "Leeuwarden": 43, "Smilde": 29, "Goes": 12, "Ignored": 123},
+    "Peter": {"Groningen": 5, "Leeuwarden": 90, "Smilde": 1, "Goes": 3, "Ignored": 123},
+    "John": {"Groningen": 50, "Leeuwarden": 43, "Smilde": 29, "Goes": 12, "Ignored": 123},
+    "Peter": {"Groningen": 5, "Leeuwarden": 90, "Smilde": 1, "Goes": 3, "Ignored": 123},
+    "John": {"Groningen": 50, "Leeuwarden": 43, "Smilde": 29, "Goes": 12, "Ignored": 123},
+    "Peter": {"Groningen": 5, "Leeuwarden": 90, "Smilde": 1, "Goes": 3, "Ignored": 123},
+    "John": {"Groningen": 50, "Leeuwarden": 43, "Smilde": 29, "Goes": 12, "Ignored": 123},
+    "Peter": {"Groningen": 5, "Leeuwarden": 90, "Smilde": 1, "Goes": 3, "Ignored": 123},
+    "John": {"Groningen": 50, "Leeuwarden": 43, "Smilde": 29, "Goes": 12, "Ignored": 123},
+    "Peter": {"Groningen": 5, "Leeuwarden": 90, "Smilde": 1, "Goes": 3, "Ignored": 123},
+    "John": {"Groningen": 50, "Leeuwarden": 43, "Smilde": 29, "Goes": 12, "Ignored": 123},
+    "Peter": {"Groningen": 5, "Leeuwarden": 90, "Smilde": 1, "Goes": 3, "Ignored": 123},
+    "John": {"Groningen": 50, "Leeuwarden": 43, "Smilde": 29, "Goes": 12, "Ignored": 123},
+    "Peter": {"Groningen": 5, "Leeuwarden": 90, "Smilde": 1, "Goes": 3, "Ignored": 123},
 }
 
 stations = list(map(stationToTensor, inputdata.keys()))
 signals = list(map(signalToTensor, inputdata.values()))
 
-dataset = tf.data.Dataset.from_tensor_slices((signals, stations))
+
+print('-------- station tensors -------------')
+print(stations)
+
+dataset = tf.data.Dataset.from_tensors((signals, stations))
+
+print('-------- dataset from signals and stations tensors -------------')
 print(dataset)
+
+
+# Split and shuffle the dataset into training, validation and testing sets
+dataset_training, dataset_validation, dataset_test = get_dataset_partitions_tf(dataset, len(signals))
+
+print('-------- training dataset -------------')
+print(dataset_training)
+
+print('-------- validation dataset -------------')
+print(dataset_validation)
+
+print('-------- test dataset -------------')
+print(dataset_test)
+
+# Create a sequential model (no idea if this is the correct one)
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Dense(len(receivers)),
+  tf.keras.layers.Dense(len(stations)*2, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(len(stations))
+])
+
+print(dataset_training)
+print('---')
+
+
+a = dataset_training.take(1)
+print(a)
+predictions = model.predict(a)
+print(predictions)
+
